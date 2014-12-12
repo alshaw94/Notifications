7917db5d746e4fa480a198be05a782e18a5ecd22
alshaw94 <shaa2_13@uni.worc.ac.uk>
2014-12-12 14:39:18 +0000

    This will post a notification after 30 minutes

--- sample code/js/index.js
+++ sample code/js/index.js
@@ -58,7 +58,7 @@ function createNotification() {
     //generate a time to post notification
     //
     var currentTime = new Date().getTime(); //current time
-    var notificationTime = new Date(currentTime + 100); //delayed time  - add 1 second
+    var notificationTime = new Date(currentTime + 1800000); //delayed time  - add 1 second
     			
     //
     //setup notification
