98a5f05c86dac4d07117b2c755de2d285b93232a
alshaw94 <shaa2_13@uni.worc.ac.uk>
2014-12-12 14:00:15 +0000

    added a comma so it works

--- sample code/js/index.js
+++ sample code/js/index.js
@@ -23,7 +23,7 @@ $(document).on('pageinit', function() {
 function createMessage(){		
 	//phoneGap and jQueryMobile do not support toast messages directly
     //so we can add this using toast.js
-    new Toast({content: 'Hello!' duration: 3000}); 	
+    new Toast({content: 'Hello!', duration: 3000}); 	
 }
         	
 
